ed841ed7240eac26830f412589ffb21a76155af4
Emilio Barrera <barrera_emilio@yahoo.es>
2025-11-29 15:39:18 +0000

    TaskListViewModel and TaskDetailViewModel done

--- TaskManager.MAUI/ViewModels/TaskDetailViewModel.cs
+++ TaskManager.MAUI/ViewModels/TaskDetailViewModel.cs
@@ -1,10 +1,98 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using TaskManager.Applications.Services;
+using TaskManager.Domain.Entities;
 
-namespace TaskManager.MAUI.ViewModels
+namespace TaskManager.MAUI.ViewModels;
+
+[QueryProperty(nameof(TaskId), "TaskId")]
+public partial class TaskDetailViewModel : ObservableObject
 {
-    public class TaskDetailViewModel
+    private readonly TaskService _taskService;
+
+    [ObservableProperty]
+    private int taskId;
+
+    [ObservableProperty]
+    private string title = string.Empty;
+
+    [ObservableProperty]
+    private string description = string.Empty;
+
+    [ObservableProperty]
+    private DateTime? dueDate;
+
+    [ObservableProperty]
+    private TaskPriority priority = TaskPriority.Medium;
+
+    [ObservableProperty]
+    private bool isEditMode;
+
+    public TaskDetailViewModel(TaskService taskService)
     {
+        this._taskService = taskService;
+    }
+
+    private void OnTaskIdChange(int value)
+    {
+        if (value > 0)
+        {
+            IsEditMode = true;
+            LoadTaskAsync(value);
+        }
+    }
+
+    private async void LoadTaskAsync(int id)
+    {
+        var task = await _taskService.GetTaskByIdAsync(id);
+        if (task != null)
+        {
+            Title = task.Title;
+            Description = task.Description;
+            DueDate = task.DueDate;
+            Priority = task.Priority;
+        }
+    }
+
+    [RelayCommand]
+    private async Task SaveTaskAsyn()
+    {
+        if (string.IsNullOrWhiteSpace(Title))
+        {
+            await Shell.Current.DisplayAlertAsync("Validation", "Please enter a title.", "OK");
+            return;
+        }
+
+        if (string.IsNullOrWhiteSpace(Description))
+        {
+            await Shell.Current.DisplayAlertAsync("Validation", "Please enter a description.", "OK");
+            return;
+        }
+
+        var task = new TaskItem
+        {
+            Id = taskId,
+            Title = Title,
+            Description = Description,
+            DueDate = DueDate,
+            Priority = priority,
+        };
+
+        var success = IsEditMode ? await _taskService.UpdateTaskAsync(task) : await _taskService.CreateTaskAsync(task);
+
+        if (success)
+        {
+            await Shell.Current.GoToAsync("..");
+        }
+        else
+        {
+            await Shell.Current.DisplayAlertAsync("Error", "Failed to save task.", "OK");
+        }
+    }
+
+    [RelayCommand]
+    private async Task CancelAsync()
+    {
+        await Shell.Current.GoToAsync("..");
     }
 }
--- TaskManager.MAUI/ViewModels/TaskListViewModel.cs
+++ TaskManager.MAUI/ViewModels/TaskListViewModel.cs
@@ -1,10 +1,91 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using TaskManager.Applications.Services;
+using TaskManager.Domain.Entities;
+using TaskManager.Infrastructure;
+using TaskManager.MAUI.Views;
 
-namespace TaskManager.MAUI.ViewModels
+namespace TaskManager.MAUI.ViewModels;
+
+public partial class TaskListViewModel : ObservableObject
 {
-    public class TaskListViewModel
+    private readonly TaskService _taskService;
+
+    [ObservableProperty]
+    private ObservableCollection<TaskItem> tasks = new ObservableCollection<TaskItem>();
+
+    [ObservableProperty]
+    private bool isRefreshing;
+
+    [ObservableProperty]
+    private bool isLoading;
+
+    public TaskListViewModel(TaskService taskService)
     {
+        this._taskService = taskService;
+    }
+
+    [RelayCommand]
+    private async Task LoadTaskAsync()
+    {
+        try
+        {
+            IsLoading = true;
+            var taskList = await _taskService.GetAllTaskAsync();
+            tasks = new ObservableCollection<TaskItem>(taskList.OrderByDescending(t => t.CreatedAt));
+        }
+        catch (Exception ex)
+        {
+            await Shell.Current.DisplayAlertAsync("Error", $"Failed to load task: {ex.Message}", "OK");
+        }
+        finally
+        {
+            IsLoading = false;
+            IsRefreshing = false;
+        }
+    }
+
+    [RelayCommand]
+    private async Task EditTaskAsync(TaskItem task)
+    {
+        var parameters = new Dictionary<string, object>
+        {
+            {"TaskId", task.Id },
+        };
+
+        await Shell.Current.GoToAsync(nameof(TaskDetailPage), parameters);
+    }
+
+    [RelayCommand]
+    private async Task DeleteTaskAsync(TaskItem task)
+    {
+        bool confirm = await Shell.Current.DisplayAlertAsync(
+             "Delete Task",
+             $"Are you sure want to delete '{task.Title}'?",
+             "Delete",
+             "Cancel");
+
+        if (confirm)
+        {
+            await _taskService.DeleteTaskAsync(task.Id);
+            await LoadTaskAsync();
+        }
+    }
+
+    [RelayCommand]
+    private async Task ToogleCompletionAsync(TaskItem task)
+    {
+        await _taskService.ToogleTaskCompletionnAsync(task.Id);
+        await LoadTaskAsync();
+    }
+
+    [RelayCommand]
+    private async Task RefreshAsync()
+    {
+        await LoadTaskAsync();
     }
 }
